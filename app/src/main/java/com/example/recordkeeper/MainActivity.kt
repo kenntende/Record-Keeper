bae54c31a726d9a822665a88f918c94a996b21e2
unknown <kdntende@gmail.com>
2026-08-18 15:24:13 +0300

    Updated app to add a toolbar and options menu

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.recordkeeper
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.commit
 import com.example.recordkeeper.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationBarView
@@ -14,11 +19,44 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         binding.bottomNav.setOnItemSelectedListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.reset_running -> {
+            Toast.makeText(this, "Reset running records", Toast.LENGTH_LONG).show()
+            true
+        }
+
+        R.id.reset_cycling -> {
+            Toast.makeText(this, "Reset cycling records", Toast.LENGTH_LONG).show()
+            true
+        }
+
+        R.id.reset_all_records -> {
+            Toast.makeText(this, "Reset all records", Toast.LENGTH_LONG).show()
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 
     private fun onCyclingClicked(): Boolean {
